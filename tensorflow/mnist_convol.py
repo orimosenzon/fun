70fb8d7afc66fef6030fa0d461466e6aa66f6c1f
Ori Mosenzon <orimosenzon@gmail.com>
2017-01-13 21:21:57 +0200

    added some code from the tutorial of tensorflow

---
+++ tensorflow/mnist_convol.py
@@ -0,0 +1,29 @@
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.global_variables_initializer())
+
+y = tf.matmul(x,W) + b
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+for i in range(1000):
+  batch = mnist.train.next_batch(100)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+  
+
+
+  
