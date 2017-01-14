a50cda55559512593c3ad02f8dca1fb27a823b83
Ori Mosenzon <orimosenzon@gmail.com>
2017-01-14 19:22:04 +0200

    wip

--- tensorflow/mnist_convol.py
+++ tensorflow/mnist_convol.py
@@ -2,6 +2,16 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
 
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+
+
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
@@ -23,7 +33,15 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 for i in range(1000):
   batch = mnist.train.next_batch(100)
   train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))  
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print('the accuracy: **',accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}),'**')
+
+
+
   
-
-
   
