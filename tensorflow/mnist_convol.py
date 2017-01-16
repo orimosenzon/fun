6b140c84266899edc0b9bded54ca044158128a27
Ori Mosenzon <orimosenzon@gmail.com>
2017-01-16 09:00:02 +0200

    wip

--- tensorflow/mnist_convol.py
+++ tensorflow/mnist_convol.py
@@ -10,6 +10,13 @@ def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
 
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
 
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
@@ -41,7 +48,15 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 print('the accuracy: **',accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}),'**')
 
+# == ==
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1,28,28,1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
 
-# == reached: Convolution and Pooling ==  
-  
+# == Second Convolutional Layer == 
