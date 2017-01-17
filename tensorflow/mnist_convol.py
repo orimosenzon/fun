7385a9445ca6de22800b8fac9d19dd31dbbbb3ce
Ori Mosenzon <orimosenzon@gmail.com>
2017-01-17 08:16:37 +0200

    wip

--- tensorflow/mnist_convol.py
+++ tensorflow/mnist_convol.py
@@ -60,3 +60,27 @@ h_pool1 = max_pool_2x2(h_conv1)
 
 
 # == Second Convolutional Layer == 
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+
+# == fully connected layer ==
+
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# == dropoupt == 
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+
+# == Readout Layer ==
+
