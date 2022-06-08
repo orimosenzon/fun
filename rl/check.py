14eb833e19b01e4ff2c877a5ffaed617e9a08531
Ori Mosenzon <ori@visionary.ai>
2022-06-08 15:25:33 +0300

    added pytorch's quickstart

--- rl/check.py
+++ rl/check.py
@@ -1,3 +1,12 @@
-import gym
 
-print(gym.__version__)
+import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# can_device_access_peer
+
+print(torch.cuda.device_count()) 
+
+# import gym
+# print(gym.__version__)
+
+
