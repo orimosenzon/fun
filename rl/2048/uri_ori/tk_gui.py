aea3e334f5f02f83a84cf4580e0f197c6fb3ab36
Ori Mosenzon <orimosenzon@gmail.com>
2022-10-22 11:43:07 +0300

    tkinter gui - wip

--- rl/2048/uri_ori/tk_gui.py
+++ rl/2048/uri_ori/tk_gui.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3 
 
+import math 
+
 import tkinter as tk
 
 from board import Board
 
+log2rgb = {
+   1: (238, 228, 218), 
+   2: (237, 224, 200), 
+   3: (242, 177, 121), 
+   4: (245, 149, 99), 
+   5: (246, 124, 95), 
+   6: (246, 94, 59), 
+   7: (237, 207, 114),  
+   8: (237, 204, 97),  
+   9: (237, 200, 80),  
+   10: (237, 197, 63),  
+   11: (237, 194, 46), 
+}
+
+
+def rgb_color(rgb):
+    return f'#%02x%02x%02x' % rgb
 
 def draw_board(): 
     brd = board.brd
@@ -11,11 +30,17 @@ def draw_board():
     offset = (WIDTH - SIZE) // 2 
     for i in range(board.n):
          for j in range(board.n):
-            x, y = offset + j * s, offset + i *s
-            canvas.create_rectangle(x, y, x+s, y+s, fill='yellow', width=1)
-            val = brd[i, j]
+            x, y = offset + j * s, offset + i *s                     
+            val = brd[i, j]            
             if val != 0: 
+                lg = int(math.log(val) / math.log(2))  
+                rgb = log2rgb.get(lg, (255, 255, 255))              
+                color = rgb_color(rgb)
+                canvas.create_rectangle(x, y, x+s, y+s, fill=color, width=1)    
                 canvas.create_text(x+s//2, y+s//2, text=str(val))
+            else: 
+                canvas.create_rectangle(x, y, x+s, y+s, width=1)    
+
 
 board = Board(4)
 board.reset()
