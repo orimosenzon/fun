028731950731922569fb0d03c875600e7235c69d
Ori Mosenzon <orimosenzon@gmail.com>
2022-10-30 08:49:27 +0200

    cross entropy agent - still wip

--- rl/2048/gym2048o/cross_entropy_agent.py
+++ rl/2048/gym2048o/cross_entropy_agent.py
@@ -1,8 +1,7 @@
-
 from collections import namedtuple
 
 import torch 
-from torch import NN as nn 
+from torch import nn
 
 Episode = namedtuple('Episode', field_names=['reward', 'steps'])
 
@@ -36,8 +35,16 @@ class CrossEntropyAgent:
         self.episodes = [] 
         self.optimizer = torch.optim.Adam(params=self.net.parameters(), lr=0.01)
         self.loss_fn = nn.CrossEntropyLoss()
+        self.sm = nn.Softmax() #dim=1)
 
-    
+
+    def _avrg_reward(self):
+        sum = 0  
+        for episode in self.episodes:
+            sum += episode.reward 
+        return sum / len(self.episodes)
+
+
     def _gain_experiance(self):
         self.episodes = [] 
         for episode_n in range(self.BATCH_SIZE):
@@ -45,8 +52,14 @@ class CrossEntropyAgent:
             total = 0 
             steps = []  
             while True: 
-                action = torch.argmax(self.net(obs))
+                inp = torch.Tensor(obs).view(self.obs_size)
+                pred = self.net(inp)
+                action = int(torch.argmax(pred))
+                # a1 = int(torch.argmax(self.sm(pred)))
+                
                 n_obs, r, done, _ = self.env.step(action)
+                if r==0:  # invalid action
+                    n_obs, r, done, _ = self.env.step(env.action_space.sample())
                 total += r 
                 episode_step = EpisodeStep(obs, action)
                 steps.append(episode_step)
@@ -58,12 +71,19 @@ class CrossEntropyAgent:
             self.episodes.append(episode)
 
 
+    def make_hot(self, action): 
+        ret = torch.zeros(4)
+        ret[action] = 1 
+        return ret
+
+
     def _train_net(self): 
         for episode in self.episodes: 
             for e_step in episode.steps: 
-                inp = torch.Tensor(e_step.obs).view(self.obs_size)
+                inp = torch.Tensor(e_step.observation).view(self.obs_size)
                 pred = self.net(inp)
                 episode_act = self.make_hot(e_step.action)
+                print(pred, episode_act)
                 self.optimizer.zero_grad()
                 loss = self.loss_fn(pred, episode_act)
                 loss.backward()
@@ -79,10 +99,20 @@ class CrossEntropyAgent:
     def train(self): 
         prev_avrg = 0 
         while True: 
-            avrg_reward = self._gain_experiance()
+            self._gain_experiance()
+            self._keep_best_episodes()
+            
+            avrg_reward = self._avrg_reward()
+            print(f'avrg_reward={avrg_reward}')
             if abs(avrg_reward - prev_avrg) < 1: 
                 break 
             prev_avrg = avrg_reward
-            self._keep_best_episodes()
+
             self._train_net()
-            
+
+
+if __name__ == '__main__':
+    from env_2048 import Env2048
+    env = Env2048(4)
+    cea = CrossEntropyAgent(env)
+    cea.train() 
