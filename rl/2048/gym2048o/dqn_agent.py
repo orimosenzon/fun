87ae7ae716cc7450e2c96bb011117aad07df2976
Ori Mosenzon <orimosenzon@gmail.com>
2022-11-24 03:42:02 +0200

    sqn: wip

--- rl/2048/gym2048o/dqn_agent.py
+++ rl/2048/gym2048o/dqn_agent.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3 
 
+import random 
+
 import torch 
 from torch import nn 
 
+
+GAMMA = 0.9 
 
 class Net(nn.Module):
     def __init__(self, n, n_actions):
@@ -24,4 +28,24 @@ class Dqn_Agent:
     def __init__(self, env): 
         self.env = env 
         self.net = self.Net(env.n, env.action_space.n)
+        self.epsilon = 1
+
+
+    def gather_exprience(self, steps = -1): 
+        o = self.env.reset()
+        steps =[]
+        while True: 
+            if random.random() < self.epsilon:
+                a = self.env.action_space.sample()
+            else:
+                action_vals = self.net(torch.FloatTensor(o)) 
+                a = torch.argmax(action_vals).item()
+
+            o1, r, d, info = self.env.step(a)
+            steps.append((o.flatten(), a, r))
+            o = o1 
+
+
+    def train(self, n_episodes=100): 
+        for ep_n in range(n_episodes): 
 
