518df6e1856168284b978006945fd333bbb09829
Ori Mosenzon <orimosenzon@gmail.com>
2023-01-25 14:55:16 +0200

    some search statistics and start nn for search train

--- rl/2048/gym2048o/search_agent.py
+++ rl/2048/gym2048o/search_agent.py
@@ -1,6 +1,53 @@
 import time 
+import collections 
+
+import torch 
+import torch.nn as nn 
 
 from env_2048 import Env2048
+
+class Net(nn.Module):
+    def __init__(self, n, n_actions=4, n_blocks=1, internal_size_f=1):
+        super().__init__()
+        n2 = n * n
+        internal_size = n2 * internal_size_f  
+        layers = collections.OrderedDict()
+        in_f, out_f = n2, internal_size
+        for i in range(n_blocks):
+            if i == n_blocks-1:
+                out_f = n_actions
+            layers[f'Linear-{i}'] = nn.Linear(in_f, out_f)
+            in_f = out_f        
+            layers[f'ReLU-{i}'] = nn.ReLU()
+
+        self.net = nn.Sequential(layers)
+
+
+    def forward(self, x): 
+        return self.net(x)
+
+
+def train(its = int(1e3)): 
+    env = Env2048(4)
+    o = env.reset()
+    net = Net(4)
+
+    LR = 1e-3
+
+    optimizer = torch.optim.Adam(params=net.parameters(), lr=LR) 
+    loss_fn = nn.MSELoss()
+
+    for i in range(1, its+1):
+        _, a = search(env, 4)
+        _,r, d, _ = env.step(a)
+
+        optimizer.zero_grad()
+        # loss = loss_fn(preds, as).... change to batch programing. gather experiance... 
+        # loss.backward() 
+        optimizer.step()
+
+
+
 
 def search(env, n):
     if n==0:
@@ -29,7 +76,19 @@ def print_valid_actions(env):
     print(s) 
 
 
-if __name__ == '__main__':
+def simulate(depth):
+    env = Env2048(4)
+    o = env.reset()
+    total_reward = 0
+    d = False
+    while not d: 
+        _, a = search(env, depth)
+        o,r, d, _ = env.step(a)
+        total_reward += r 
+    return total_reward
+
+
+def graphic_simulate(depth=4):
     env = Env2048(4)
     o = env.reset()
     total_reward = 0
@@ -37,11 +96,24 @@ if __name__ == '__main__':
     while not d: 
         env.render()
         # time.sleep(0.05)
-        _, a = search(env, 4)
+        # env.render_text()
+        _, a = search(env, depth)
+        # print(env.action2str[a])
         o,r, d, _ = env.step(a)
         total_reward += r 
-        # print_valid_actions(env)
-        # print(env.action2str[a])
     print(f'{total_reward=}')
+    env.render()
     input('press any key')
 
+
+if __name__ == '__main__':
+    # graphic_simulate(1)
+
+    N = 20 
+    for depth in range(1, 8):
+        total = 0 
+        for i in range(1, N+1): 
+            score = simulate(depth)
+            print(f'{i=} {score=}')
+            total += score
+        print(f'** avarage {depth}  = {total/N} \n\n')
